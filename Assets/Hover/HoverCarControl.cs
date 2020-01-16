56f203db43b706b8261a176cd8f67ded3776b481
Lukasz Broll <lukasz@broll.pl>
2020-01-16 20:06:21 +0100

    project

--- Assets/Hover/HoverCarControl.cs
+++ Assets/Hover/HoverCarControl.cs
@@ -1,44 +1,119 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Car1_controller : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class HoverCarControl : MonoBehaviour
 {
+  Rigidbody m_body;
+  float m_deadZone = 0.1f;
 
-	private Animation anim;
-	//public Score score;
-	public Text scoreText;
+  public float m_hoverForce = 9.0f;
+  public float m_hoverHeight = 2.0f;
+  public GameObject[] m_hoverPoints;
 
+  public float m_forwardAcl = 100.0f;
+  public float m_backwardAcl = 25.0f;
+  float m_currThrust = 0.0f;
 
-	private Rigidbody rb;
-	public float Speed = 90.0f;
-	public float rotationSpeed = 45.0f;
+  public float m_turnStrength = 10f;
+  float m_currTurn = 0.0f;
 
-	private float referenceYPosition;
-	// Use this for initialization
-	void Start()
-	{
-		Resources.UnloadUnusedAssets();
-		//anim = GetComponent<Animation> ();
-		rb = GetComponent<Rigidbody>();
-		scoreText = GameObject.Find("Scoretext").GetComponent<Text>();
-		referenceYPosition = transform.localPosition.y;
+  public GameObject m_leftAirBrake;
+  public GameObject m_rightAirBrake;
 
-	}
+  int m_layerMask;
 
-	// Update is called once per frame
-	void Update()
-	{
+  void Start()
+  {
+    m_body = GetComponent<Rigidbody>();
 
-		float x = CrossPlatformInputManager.GetAxis("Horizontal");
-		float y = CrossPlatformInputManager.GetAxis("Vertical");
+    m_layerMask = 1 << LayerMask.NameToLayer("Characters");
+    m_layerMask = ~m_layerMask;
+  }
 
+  void OnDrawGizmos()
+  {
 
+    //  Hover Force
+    RaycastHit hit;
+    for (int i = 0; i < m_hoverPoints.Length; i++)
+    {
+      var hoverPoint = m_hoverPoints [i];
+      if (Physics.Raycast(hoverPoint.transform.position, 
+                          -Vector3.up, out hit,
+                          m_hoverHeight, 
+                          m_layerMask))
+      {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(hoverPoint.transform.position, hit.point);
+        Gizmos.DrawSphere(hit.point, 0.5f);
+      } else
+      {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(hoverPoint.transform.position, 
+                       hoverPoint.transform.position - Vector3.up * m_hoverHeight);
+      }
+    }
+  }
+	
+  void Update()
+  {
 
-		transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
-		transform.Translate(0, 0, y * Time.deltaTime * Speed);
+    // Main Thrust
+    m_currThrust = 0.0f;
+    float aclAxis = Input.GetAxis("Vertical");
+    if (aclAxis > m_deadZone)
+      m_currThrust = aclAxis * m_forwardAcl;
+    else if (aclAxis < -m_deadZone)
+      m_currThrust = aclAxis * m_backwardAcl;
 
-	}
+    // Turning
+    m_currTurn = 0.0f;
+    float turnAxis = Input.GetAxis("Horizontal");
+    if (Mathf.Abs(turnAxis) > m_deadZone)
+      m_currTurn = turnAxis;
+  }
+
+  void FixedUpdate()
+  {
+
+    //  Hover Force
+    RaycastHit hit;
+    for (int i = 0; i < m_hoverPoints.Length; i++)
+    {
+      var hoverPoint = m_hoverPoints [i];
+      if (Physics.Raycast(hoverPoint.transform.position, 
+                          -Vector3.up, out hit,
+                          m_hoverHeight,
+                          m_layerMask))
+        m_body.AddForceAtPosition(Vector3.up 
+          * m_hoverForce
+          * (1.0f - (hit.distance / m_hoverHeight)), 
+                                  hoverPoint.transform.position);
+      else
+      {
+        if (transform.position.y > hoverPoint.transform.position.y)
+          m_body.AddForceAtPosition(
+            hoverPoint.transform.up * m_hoverForce,
+            hoverPoint.transform.position);
+        else
+          m_body.AddForceAtPosition(
+            hoverPoint.transform.up * -m_hoverForce,
+            hoverPoint.transform.position);
+      }
+    }
+
+    // Forward
+    if (Mathf.Abs(m_currThrust) > 0)
+      m_body.AddForce(transform.forward * m_currThrust);
+
+    // Turn
+    if (m_currTurn > 0)
+    {
+      m_body.AddRelativeTorque(Vector3.up * m_currTurn * m_turnStrength);
+    } else if (m_currTurn < 0)
+    {
+      m_body.AddRelativeTorque(Vector3.up * m_currTurn * m_turnStrength);
+    }
+  }
 }
---
+++ Assets/StartFinishTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFinishTrigger : MonoBehaviour
+{
+
+    private float startTime;
+    private float ellpasedTime;
+    private bool start = false;
+    private int count = 0;
+    // Use this for initialization
+    void TimerStart()
+    {
+        startTime = Time.time;
+    }// Update is called once per frame
+    void Update()
+    {
+        if (start == true)
+        {
+            ellpasedTime = Time.time - startTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (count == 0)
+        {
+            Debug.Log("Start line went in");
+            TimerStart();
+            ++count;
+            start = true;
+        }
+        else
+        {
+            Debug.Log("End line went in");
+            ellpasedTime = Time.time - startTime;
+            count = 0;
+            start = false;
+        }
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(300, 100, 100, 20), (ellpasedTime.ToString()));
+    }
+}
